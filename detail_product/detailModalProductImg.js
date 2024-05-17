7d4cdc800d830f75cbb0de2b9e220e79ffdeaafe
nguyennhatquang522004 <nguyennhatquang522004@gmail.com>
2024-05-18 04:44:50 +0700

    asd

--- detail_product/detailModalProductImg.js
+++ detail_product/detailModalProductImg.js
@@ -9,30 +9,11 @@ const changeImg = (btnOpenModalProductDetailImg, ImgOutSide) => {
         })
     })
 }
-const handleShowItem_Color = async () => {
-    const FetchItem = await fetch.getData(`./Color.json`);
-    if (FetchItem) {
-        $('#WrapColorRightGird').empty().append(FetchItem.data.data.map(item =>
-            `<div class="wrap_item border border-1 mx-2  mt-3 wrap_item-color" >
-            <div class="wrap_item-gird row py-2 px-1  align-items-center mx-0 ">
-                <img class="item-img  col-2 px-0"
-                    src="${item.url}"
-                    alt="">
-                <span
-                    class="item-title text-capitalize col-10 px-0 ps-2 text-uppercase text-truncate">${item.title}
-             </span>
-             <span class="item-type"  id="${item.type}"></span>
-            </div>
-        </div>`
-        ))
-    }
-}
-const handleChangeImg_color = (btnOpenModalProductDetailImg) => {
-    const itemColor = $('#WrapColorRightGird').find('.wrap_item-color')
-    const parrenItemColor = $('#WrapColorRightGird')
+
+const handleChangeImg_color = (btnOpenModalProductDetailImg, itemColor) => {
     $(itemColor).mouseover(function () {
         const position = itemColor.index($(this))
-        btnOpenModalProductDetailImg.attr("src", `${$(itemColor[position]).find(".item-img").attr('src')}`)
+        btnOpenModalProductDetailImg.attr("src", `${$(itemColor[position]).find(".wrap_item-gird .item-img").attr('src')}`)
     })
 }
 const scrollslidecart = () => {
@@ -125,39 +106,91 @@ const handleOpenClose_ModalProducts = (btnOpenModalProductDetailImg, modalProduc
         $(representItemImg[CountNextAndPrevios_ModalProducts.Count]).addClass('input-red-boder')
     })
 }
-const handleShowInitial_ModalProducts = (ImgOutSide, btnOpenModalProductDetailImg, itemImgMain) => {
+const handleShowInitial_ModalProducts = (ImgOutSide, btnOpenModalProductDetailImg, itemImgMain, typeColor) => {
+    typeColor.removeClass('text-grey')
     $(ImgOutSide[0]).addClass('input-red-boder')
     btnOpenModalProductDetailImg.attr('src', `${$(ImgOutSide[0]).attr('src')}`)
     itemImgMain.attr('src', `${$(ImgOutSide[0]).attr('src')}`)
 }
+const handleShow_Type = async () => {
+    const listType = await fetch.getData('./Type.json')
+    const list = listType ? listType.data.data : []
+    $("#WrapTypeRightGird").empty().append(list.map(item =>
+        `<div class="wrap_item border border-1   mx-2 mt-2 wrap_item-type" >
+<div
+    class="wrap_item-container container px-3 py-2 position-relative">
+${item.title}
+    <span class="type" id="${item.id}">${item.type}</span>
+</div>
+</div>`
+    ))
+
+
+}
+const handleShowItem_Color = async () => {
+    const FetchItem = await fetch.getData(`./Color.json`);
+    if (FetchItem) {
+        $('#WrapColorRightGird').empty().append(FetchItem.data.data.map(item =>
+            `<div class="wrap_item border border-1 mx-2  mt-3 wrap_item-color" >
+            <div class="wrap_item-gird row py-2 px-1  align-items-center mx-0 ">
+                <img class="item-img  col-2 px-0"
+                    src="${item.url}"
+                    alt="">
+                <span
+                    class="item-title text-capitalize col-10 px-0 ps-2 text-uppercase text-truncate">${item.title}
+             </span>
+             <span class="item-type"  id="${item.id}">${item.type}</span>
+            </div>
+        </div>`
+        ))
+    }
+}
+const handleClick_type = (parentItemType, typeColor, typeItem, itemTitleColor) => {
+    parentItemType.click(function () {
+        const position = parentItemType.index(this)
+        parentItemType.removeClass("input-red-text input-red-boder");
+        $(this).addClass('input-red-text input-red-boder')
+        typeColor.each((index, item) => {
+            $(item).parent().parent().removeClass('item-Color-Action')
+            if ($(typeItem[position]).text() === $(item).text()) {
+                $(item).parent().parent().addClass("item-Color-Action");
+            }
+        })
+    })
+}
 const CountNextAndPrevios_ModalProducts = {
     Count: 0,
 }
-const handleClick_type = () => {
-
-}
-export const AllHandleModalProductsImg = async () => {
+export const AllHandleModalProductsImg = () => {
     handleShowItem_ModalProducts()
+
     handleShowTitle_ModalProducts()
+    handleShow_Type();
     handleShowItem_Color();
     setTimeout(() => {
-        let modalProductDetailImg = $('#ModalProductsDetailImg')
-        let btnOpenModalProductDetailImg = $('#OpenModalProductsDetailImg')
-        let btnNextModalProductDetailImg = $('#ModalProductsDetailImgButtonNext')
-        let btnPreviosModalProductDetailImg = $('#ModalProductsDetailImgButtonPrevious')
-        let itemImgMain = $('#ModalProductsDetailImgDisplayMain')
-        let ImgOutSide = $('#DetailImgGird .item-img')
-        let manyItemImg = $('#ModalProductsDetailWrapImgGrid .wrap-item').find('.ModalProductsDetail_item-img')
-        let representItemImg = $('#ModalProductsDetailWrapImgGrid .wrap-item').find('.curtainlayer-ModalProductsDetailImg')
-        handleShowInitial_ModalProducts(ImgOutSide, btnOpenModalProductDetailImg, itemImgMain)
+        const modalProductDetailImg = $('#ModalProductsDetailImg')
+        const btnOpenModalProductDetailImg = $('#OpenModalProductsDetailImg')
+        const btnNextModalProductDetailImg = $('#ModalProductsDetailImgButtonNext')
+        const btnPreviosModalProductDetailImg = $('#ModalProductsDetailImgButtonPrevious')
+        const itemImgMain = $('#ModalProductsDetailImgDisplayMain')
+        const ImgOutSide = $('#DetailImgGird .item-img')
+        const manyItemImg = $('#ModalProductsDetailWrapImgGrid .wrap-item').find('.ModalProductsDetail_item-img')
+        const representItemImg = $('#ModalProductsDetailWrapImgGrid .wrap-item').find('.curtainlayer-ModalProductsDetailImg')
+        const itemColor = $('#WrapColorRightGird').find('.wrap_item-color')
+        const itemTitleColor = $('#WrapColorRightGird').find('.wrap_item-color .wrap_item-gird .item-title')
+        const parentItemType = $('#WrapTypeRightGird').find('.wrap_item-type');
+        const typeColor = $('#WrapColorRightGird').find('.wrap_item-color .wrap_item-gird .item-type')
+        const typeItem = $('#WrapTypeRightGird').find('.wrap_item-type .wrap_item-container .type');
+        handleShowInitial_ModalProducts(ImgOutSide, btnOpenModalProductDetailImg, itemImgMain, typeColor)
         handleOpenClose_ModalProducts(btnOpenModalProductDetailImg, modalProductDetailImg, representItemImg, ImgOutSide, itemImgMain)
         handleSetupName_ModalProducts(modalProductDetailImg)
         changeImg(btnOpenModalProductDetailImg, ImgOutSide);
         handleButtonNext_ModalProducts(btnNextModalProductDetailImg, btnPreviosModalProductDetailImg, itemImgMain, manyItemImg, representItemImg)
         handleClick_ModalProducts(itemImgMain, manyItemImg, representItemImg);
         scrollslidecart();
-        handleChangeImg_color(btnOpenModalProductDetailImg);
-    }, 500);
+        handleChangeImg_color(btnOpenModalProductDetailImg, itemColor);
+        handleClick_type(parentItemType, typeColor, typeItem, itemTitleColor);
+    }, 1000);
 }
 
 
